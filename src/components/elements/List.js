eb3da880375b93e2089bbb021f9b9dc80249ec80
DeMoorJasper <jasperdemoor@gmail.com>
2018-07-29 18:55:48 -0700

    add list and fix some stuff

--- src/components/elements/List.js
+++ src/components/elements/List.js
@@ -5,5 +5,12 @@ export const List = styled.ul`
 `;
 
 export const ListItem = styled.li`
-  
+  font-size: 3rem;
+  text-transform: uppercase;
+  list-style-type: none;
+  margin: 1rem auto;
+
+  &:before {
+    content: "-  ";
+  }
 `;
--- src/components/slides/Slide4.js
+++ src/components/slides/Slide4.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Slide from '../slideTemplates/Slide';
+import {SubHeading} from '../elements/Heading';
+import {List, ListItem} from '../elements/List';
 
 module.exports = () => {
   return <Slide>
-    Hey
+    <SubHeading>
+      Lists of bulletpoints
+    </SubHeading>
+    <List>
+      <ListItem>Item 1</ListItem>
+      <ListItem>Item 2</ListItem>
+      <ListItem>Item 3</ListItem>
+      <ListItem>Item 4</ListItem>
+      <ListItem>Item 5</ListItem>
+    </List>
   </Slide>;
 }
